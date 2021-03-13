0c4d0fb068f0b68b91b2e5d0760fd05d04b6593e
dmitri9149 <dmitri.as.name@gmail.com>
2021-03-13 15:19:02 +0200

    some development

--- src/bin/en_tokenizer.rs
+++ src/bin/en_tokenizer.rs
@@ -3,8 +3,10 @@ use entropy_tokenizer::text_stage::{TextStageOriginal, TextStage};
 
 fn main() {
 // get text from a file 
-    let txt = TextStageOriginal::build_text_stage("data/Ulysses.txt");
-    let txt_nxt = TextStage::from_original(&txt);
-    println!("The text : {:?}\n", txt_nxt.text);
+    let txt_orig = TextStageOriginal::build_text_stage("data/Ulysses.txt");
+    let mut txt = TextStage::from_original(&txt_orig);
+    txt = txt.replace_string_to_string("\r\n"," ");
+
+    println!("The text : {:?}\n", txt.text);
 }
 
