78ac19865f9cad872711445eb1b5405dfdb7e58e
dmitri9149 <dmitri.as.name@gmail.com>
2021-03-14 17:16:17 +0200

    some development in sentence processing

--- src/bin/en_tokenizer.rs
+++ src/bin/en_tokenizer.rs
@@ -1,7 +1,7 @@
 // main functionality of the tokenizer
 use entropy_tokenizer::text_stage::{TextStageOriginal, TextStage};
 use entropy_tokenizer::sentence_stage::Sentences;
-use entropy_tokenizer::sentence_stage_a::SentencesA;
+//use entropy_tokenizer::sentence_stage_a::SentencesA;
 
 
 
@@ -11,8 +11,9 @@ fn main() {
     let txt_orig = TextStageOriginal::build_text_stage("data/Ulysses.txt");
     let mut txt = TextStage::from_original(&txt_orig);
     txt = txt.replace_string_to_string("\r\n"," ");
-    let mut sentences = SentencesA::vocab_of_sentences_a(&txt);
-    sentences.trim_sentences_a('A');
+    let mut sentences = Sentences::vocab_of_sentences(&txt);
+    sentences.trim_sentences(' ');
+    sentences.separate_punctuation("?.!\"—,_:");
 
 //    println!("The text : {:?}\n", txt.text);
     println!("The sentences : {:?}", &sentences.sentences[0..100]);
