a02b9f21f5b7cf5bfc0e5886ea5eb921fff117b6
dmitri9149 <dmitri.as.name@gmail.com>
2021-03-14 17:12:51 +0200

    try to make some tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,24 @@
 pub mod text_stage;
 pub mod sentence_stage;
-pub mod sentence_stage_a;
+//pub mod sentence_stage_a;
 pub mod string_processing;
 pub use crate::text_stage as text;
 pub use crate::sentence_stage as sentence;
-pub use crate::sentence_stage_a as sentence_a;
+//pub use crate::sentence_stage_a as sentence_a;
+pub use crate::string_processing as str_proc;
 
+/*
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn split_punctuation() {
+        let sentence: &mut str = "some! text to U.S.A to test.";
+        let result = "some !  text to U . S . A to test . ";
+        assert_eq!(result, str_proc::separate_punctuation(sentence,"!.?"));
+
+    }
+}
+
+*/
