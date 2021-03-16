26d19180adb24f94ec43ff2f654f7b709e2e97bd
dmitri9149 <dmitri.as.name@gmail.com>
2021-03-16 12:08:24 +0200

    some changes

--- src/bin/en_tokenizer.rs
+++ src/bin/en_tokenizer.rs
@@ -1,6 +1,7 @@
 // main functionality of the tokenizer
 use entropy_tokenizer::text_stage::{TextStageOriginal, TextStage};
 use entropy_tokenizer::sentence_stage::{Sentences, WordsCollection};
+use entropy_tokenizer::string_processing::{to_collection_split_on_space};
 //use entropy_tokenizer::sentence_stage_a::SentencesA;
 
 
@@ -12,6 +13,9 @@ fn main() {
     let mut txt = TextStage::from_original(&txt_orig);
     txt = txt.replace_string_to_string("\r\n"," ");
     let mut sentences = Sentences::vocab_of_sentences(&txt);
+
+//    println!("The text sentences: {:?}\n", &sentences.sentences[0..200]);
+
     sentences.trim_sentences(' ');
     sentences.separate_punctuation("?.!\"—,_:");
     sentences.split_on_string(",");
@@ -25,12 +29,15 @@ fn main() {
     sentences.trim_sentences(' ');
     sentences.no_empty_strings();
 
+    let collection = WordsCollection::from_sentences(&sentences, to_collection_split_on_space);
+
 
     
 
 
 
 //    println!("The text : {:?}\n", txt.text);
-    println!("The sentences : {:?}", &sentences.sentences[0..200]);
+    println!("The sentences :\n{:?}\n", &sentences.sentences[0..200]);
+    println!("The collections:\n{:?}\n", &collection.collections[0..200]);
 }
 
